165db907457a4891b1b31232ef618fdc7a13ddc5
Rezaul Khan <73153481+Rkhan77@users.noreply.github.com>
2026-06-20 21:01:49 +0800

    Update index.tsx

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -105,7 +105,7 @@ const ExternalProjectCard = ({
                           alt={'thumbnail'}
                           placeholder={skeleton({
                             widthCls: 'w-full',
-                            heightCls: 'h-24',
+                            heightCls: 'h-auto',
                             shape: '',
                           })}
                         />
